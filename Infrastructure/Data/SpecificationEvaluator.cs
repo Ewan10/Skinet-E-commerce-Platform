0cd2d339616fe7bf3d07a78ccbffcee18b9005a0
Ioannis <secgeorgiou@hotmail.com>
2026-05-02 20:14:44 +0300

    End of sec 5

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -27,6 +27,11 @@ public class SpecificationEvaluator<T> where T : class
             query = query.Distinct();
         }
 
+        if (spec.IsPagingEnabled)
+        {
+            query = query.Skip(spec.Skip).Take(spec.Take);
+        }
+
         return query;
     }
 
@@ -59,6 +64,11 @@ public class SpecificationEvaluator<T> where T : class
         if (spec.IsDistinct)
         {
             selectQuery = selectQuery?.Distinct();
+        }
+
+        if (spec.IsPagingEnabled)
+        {
+            selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
         }
 
         return selectQuery ?? query.Cast<TResult>();
