b27dc3e5e8b3ea2f257e0cb5a28a6b57bedd69fc
Ioannis <secgeorgiou@hotmail.com>
2026-05-06 16:02:51 +0300

    End of sec 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,15 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient } from '@angular/common/http';
-import { Product } from './shared/models/product';
-import { Pagination } from './shared/models/Pagination';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-  protected readonly title = signal('client');
-  products: Product[] = [];
+export class AppComponent {
   
-  ngOnInit(): void {
-      this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
-        next: response => this.products = response.data,
-        error: (error) => console.error(error),
-        complete: () => console.log('Request completed')
-      })
-  }
 
 
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,12 +2,17 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: { autoFocus: 'dialog', restoredFocus: true }
+    }
   ]
 };
